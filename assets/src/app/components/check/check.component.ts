a8e3ea80a7c4194337f550dbfbccf9c7ef7055c3
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2021-07-01 20:57:06 +0700

    u

--- assets/src/app/components/check/check.component.ts
+++ assets/src/app/components/check/check.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-check',
   templateUrl: './check.component.html',
-  styleUrls: ['./check.component.css']
+  styleUrls: ['./check.component.css'],
 })
 export class CheckComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  mainViwe: boolean = true;
+  kem: boolean = false;
+  kemMe: any = 1;
   ngOnInit(): void {
+    this.mainViwe;
   }
 
+  openKem1 = () => {
+    this.mainViwe = false;
+    this.kem = true;
+    this.kemMe = 1;
+  };
+  openKem2 = () => {
+    this.mainViwe = false;
+    this.kem = true;
+    this.kemMe = 2;
+  };
+  backMain = () => {
+    this.mainViwe = true;
+    this.kem = false;
+  };
+  backMainCancel = () =>{
+    this.mainViwe = true;
+    this.kem = false;
+  }
 }
